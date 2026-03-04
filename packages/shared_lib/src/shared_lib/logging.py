239cb8cfa558030d4e010b9fb6b7b844e237bc09
christianfm10 <christian.flores@dekoding.pe>
2026-03-04 10:43:46 -0500

    Added LogFile handlers

--- packages/shared_lib/src/shared_lib/logging.py
+++ packages/shared_lib/src/shared_lib/logging.py
@@ -1,12 +1,20 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from rich.logging import RichHandler
 
 
-def setup_logging():
+def setup_logging(log_file: bool = False, log_name: str = "temp.log"):
+    handlers = [RichHandler()]
+    if log_file:
+        rotatating_handler = RotatingFileHandler(
+            log_name, maxBytes=5 * 1024 * 1024, backupCount=1, encoding="utf-8"
+        )
+        rotatating_handler.setLevel(logging.INFO)
+        handlers.append(rotatating_handler)
     logging.getLogger("nodriver").setLevel(logging.WARNING)
     logging.getLogger("httpx").setLevel(logging.WARNING)
     logging.basicConfig(
         level=logging.INFO,
         # format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[RichHandler()],
+        handlers=handlers,
     )
