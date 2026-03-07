228892091a4a06f8f4346faf3f0ec929164bd2f0
christianfm10 <christian.flores@dekoding.pe>
2026-03-07 00:38:19 -0500

    UserCreatedCoinDB use mapped_column

--- packages/pumpfun/src/pumpfun/entities/user_created_coins.py
+++ packages/pumpfun/src/pumpfun/entities/user_created_coins.py
@@ -1,14 +1,8 @@
+from datetime import datetime
+
 from shared_lib.database.base import Base
-from sqlalchemy import (
-    Boolean,
-    Column,
-    DateTime,
-    Float,
-    Index,
-    Integer,
-    String,
-    func,
-)
+from sqlalchemy import Index, func
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class UserCreatedCoinDB(Base):
@@ -16,28 +10,28 @@ class UserCreatedCoinDB(Base):
 
     __tablename__ = "user_created_coins"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    mint = Column(String, nullable=False, unique=True, index=True)
-    name = Column(String, nullable=False)
-    symbol = Column(String, nullable=False)
-    bonding_curve = Column(String, nullable=False)
-    creator = Column(String, nullable=False, index=True)
-    complete = Column(Boolean, nullable=False, default=False)
-    ath_market_cap = Column(Float, nullable=True)
-    ath_market_cap_timestamp = Column(Integer, nullable=True)
-    created_timestamp = Column(Integer, nullable=True)
-    description = Column(String, nullable=True)
-    banner_uri = Column(String, nullable=True)
-    website = Column(String, nullable=True)
-    twitter = Column(String, nullable=True)
-    metadata_uri = Column(String, nullable=True)
-    init_price = Column(Float, nullable=True)
-    is_active = Column(Boolean, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    mint: Mapped[str] = mapped_column(unique=True, index=True)
+    name: Mapped[str]
+    symbol: Mapped[str]
+    bonding_curve: Mapped[str]
+    creator: Mapped[str] = mapped_column(index=True)
+    complete: Mapped[bool] = mapped_column(default=False)
+    ath_market_cap: Mapped[float | None]
+    ath_market_cap_timestamp: Mapped[int | None]
+    created_timestamp: Mapped[int | None]
+    description: Mapped[str | None]
+    banner_uri: Mapped[str | None]
+    website: Mapped[str | None]
+    twitter: Mapped[str | None]
+    metadata_uri: Mapped[str | None]
+    init_price: Mapped[float | None]
+    is_active: Mapped[bool | None]
 
     # Timestamps
-    inserted_at = Column(DateTime, default=func.now(), nullable=False)
-    updated_at = Column(
-        DateTime, default=func.now(), onupdate=func.now(), nullable=False
+    inserted_at: Mapped[datetime] = mapped_column(default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), onupdate=func.now()
     )
 
     __table_args__ = (
